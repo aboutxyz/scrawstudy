9306099f01f9cb7eabf992129974d2d1d39a2322
aboutxyz <haishangmuxucao@dlmu.edu.cn>
2016-12-01 22:30:19 +0800

    add a future

--- test.py
+++ test.py
@@ -14,46 +14,38 @@ from optparse import OptionParser
 import concurrent.futures
 
 seen_urls = set(['/'])
-class Fetch():
-    def __init__(self, *kwargs):
-        self.url = None
-        self.responseurl = None
+seturl = set()
 
-    def run(self, url):
+def parse(fetched_url):
+    try:
         s = requests.session()
-        self.responseurl = s.get("http://localhost:3000/" + self.url, timeout=0.1)
-        self.responseurl.encoding = "utf-8"
-        if self.responseurl is not None:
-            links = self.parse(self, self.url, self.responseurl)
-            for link in links.difference(seen_urls):
-                self.tasks.put((link, self.depth))
-            seen_urls.update(links)
-        return seen_urls
-            
-            
-    @staticmethod
-    def parse(self, fetched_url, responseurl):
-        seturl = set()
-        try:
-            selector = etree.HTML(responseurl.content)
-            urls = set(selector.xpath('//a/@href'))
-        except KeyError:
-            pass
-        for url in urls:
-            normalized = urlparse.urljoin(fetched_url, url)
-            parts = urlparse.urlparse(normalized)
-            if parts.scheme not in ('', 'http', 'https'):
-                continue
-            host, port = parts.hostname, parts.port
-            if host and host.lower() not in ('localhost'):
-                continue
-            defragmented, frag = urlparse.urldefrag(parts.path)
-            seturl.add(defragmented)
-        return seturl
-    
-    
+        responseurl = s.get("http://localhost:3000/" + fetched_url, timeout=0.1)
+        responseurl.encoding = "utf-8"
+    except requests.RequestException as e:
+        pass
+    try:
+        selector = etree.HTML(responseurl.content)
+        urls = set(selector.xpath('//a/@href'))
+    except KeyError:
+        pass
+    for url in urls:
+        normalized = urlparse.urljoin(fetched_url, url)
+        parts = urlparse.urlparse(normalized)
+        if parts.scheme not in ('', 'http', 'https'):
+            continue
+        host, port = parts.hostname, parts.port
+        if host and host.lower() not in ('localhost'):
+            continue
+        defragmented, frag = urlparse.urldefrag(parts.path)
+        seturl.add(defragmented)
+    seen_urls.update(seturl)
+    return seen_urls
+
 
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
-    result = executor.map(Fetch().run, seen_urls)
-    for i in result:
-        print i
+    future_to_url = executor.submit(parse, '/')
+    for i in range(3):
+        for url in list(future_to_url.result()):
+            future_to_url = executor.submit(parse, url)
+    print len(future_to_url.result())
+
